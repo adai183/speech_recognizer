22674c7f30998bd9d76f690c533cfc7cced39621
adai183 <andreas.daiminger@gmail.com>
2018-10-23 11:50:02 +0000

    init

--- sample_models.py
+++ sample_models.py
@@ -28,9 +28,9 @@ def rnn_model(input_dim, units, activation, output_dim=29):
     simp_rnn = GRU(units, activation=activation,
         return_sequences=True, implementation=2, name='rnn')(input_data)
     # TODO: Add batch normalization 
-    bn_rnn = ...
+    bn_rnn = BatchNormalization()(simp_rnn)
     # TODO: Add a TimeDistributed(Dense(output_dim)) layer
-    time_dense = ...
+    time_dense = TimeDistributed(Dense(output_dim))(bn_rnn)
     # Add softmax activation layer
     y_pred = Activation('softmax', name='softmax')(time_dense)
     # Specify the model
@@ -58,9 +58,9 @@ def cnn_rnn_model(input_dim, filters, kernel_size, conv_stride,
     simp_rnn = SimpleRNN(units, activation='relu',
         return_sequences=True, implementation=2, name='rnn')(bn_cnn)
     # TODO: Add batch normalization
-    bn_rnn = ...
+    bn_rnn = BatchNormalization(name='bn_rnn')(simp_rnn)
     # TODO: Add a TimeDistributed(Dense(output_dim)) layer
-    time_dense = ...
+    time_dense = TimeDistributed(Dense(output_dim))(bn_rnn)
     # Add softmax activation layer
     y_pred = Activation('softmax', name='softmax')(time_dense)
     # Specify the model
@@ -97,10 +97,26 @@ def deep_rnn_model(input_dim, units, recur_layers, output_dim=29):
     """
     # Main acoustic input
     input_data = Input(name='the_input', shape=(None, input_dim))
+    
     # TODO: Add recurrent layers, each with batch normalization
-    ...
+    simp_rnn_0 = GRU(units, activation='relu',
+        return_sequences=True, implementation=2, name='rnn_0')(input_data)
+    bn_rnn_0 = BatchNormalization(name='bn_rnn_0')(simp_rnn_0)
+    
+    simp_rnn_0 = GRU(units, activation='relu', 
+                             return_sequences=True, implementation=2, name='rnn_0')(input_data)
+    bn_rnn_0 = BatchNormalization(name='bn_rnn_0')(simp_rnn_0)
+    
+    prev = bn_rnn_0
+    for i in range(recur_layers-1):
+        simp_rnn_i = GRU(units, activation='relu', 
+                             return_sequences=True, implementation=2, name=f'rnn_{str(i+1)}')(prev)
+        bn_rnn_i = BatchNormalization(name=f'bn_rnn_{str(i+1)}')(simp_rnn_i)
+        prev = bn_rnn_i
+        
+      
     # TODO: Add a TimeDistributed(Dense(output_dim)) layer
-    time_dense = ...
+    time_dense = TimeDistributed(Dense(output_dim), name="time_dense")(prev)
     # Add softmax activation layer
     y_pred = Activation('softmax', name='softmax')(time_dense)
     # Specify the model
